5428edf4c1521c16db9b802fb0a7aa410f47fffd
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-04-29 22:53:50 +0530

    gotta study about this

--- linked-list.js
+++ linked-list.js
@@ -1,1 +1,25 @@
 'use strict';
+
+class Node {
+    constructor(item, next) {
+        this.data = item;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.next = null;
+        this.start = 0;
+        this.data = [];
+    }
+    insertNodeAtTheEnd(item) {
+        let newNode = new Node(item, null);
+
+    }
+    deleteNode() {}
+    display() {}
+    isEmpty() {
+        return this.start === 0;
+    }
+}
